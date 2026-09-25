65ce0ffb0ef25bd1c288d5c4fb5699123ac216e8
pramath-srivastava <srivastavpramath09@gmail.com>
2026-09-25 23:08:25 +0530

    first

---
+++ script.js
@@ -0,0 +1,5 @@
+// This is a simple message printed to the browser's developer console
+console.log("Hello! Your script.js file is connected successfully.");
+
+// This creates an interactive popup alert on the screen
+alert("Welcome to my website!");
