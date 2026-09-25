beb714d6902aacea2d72ccdcaba439e4d1e9afa4
pramath-srivastava <srivastavpramath09@gmail.com>
2026-09-25 23:32:20 +0530

    Initial commit

--- script.js
+++
@@ -1,5 +0,0 @@
-// This is a simple message printed to the browser's developer console
-console.log("Hello! Your script.js file is connected successfully.");
-
-// This creates an interactive popup alert on the screen
-alert("Welcome to my website!");
